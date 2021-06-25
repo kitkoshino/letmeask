feb53afeda5b7772e9b98b0f4b576d68b2bfcb1f
cristianekoshino <cristianekoshino@gmail.com>
2021-06-26 00:52:06 +0100

    add questions actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { AdminRoom } from './pages/AdminRoom';
 import {AuthContextProvider} from './contexts/AuthContext';
 import { Room } from './pages/Room';
 import './styles/global.scss';
@@ -16,6 +17,9 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,14 @@ import { ButtonHTMLAttributes } from 'react';
 import '../../styles/button.scss';
 
 //tipagem ButtonHTMLAttributes aceita como "propriedade" toda prop. do elemento button do HTML
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({isOutlined = false, ...props}: ButtonProps) => {
   return (
-    <button className="button" {...props} />
+    <button
+      className={`button ${isOutlined ? 'outlined' : ''}`} {...props} />
   )
 };
 
